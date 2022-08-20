46bf883ccb46ee5a7daa9dc3c976c3054fc55381
MrMcHir <102164593+MrMcHir@users.noreply.github.com>
2022-08-20 18:55:26 +0600

    Update premium.lua

--- Library/premium.lua
+++ Library/premium.lua
@@ -1,7 +1,7 @@
 userids = {
   [2309893201] = true, -- Xrer
   [929659212] = true, -- Touk
-  [546967671] = true -- /.\
+  [546967671] = true, -- /.\
   [36459721] = true -- Negromaster2008
 }
 return userids
